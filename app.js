5a1ba04a0e3f967ff65d901713dd1cb4211fe29a
harrahx3 <hyacinthe.menard@gmail.com>
2021-01-12 12:07:30 +0100

    commit relou from lenovo

--- app.js
+++ app.js
@@ -6,81 +6,10 @@ const https = require("https");
 const fs = require("fs");
 var favicon = require('serve-favicon');
 const webpush = require('web-push');
-
 const path = require('path');
 const bodyParser = require("body-parser");
+var request = require('request');
 
-//var https = require('follow-redirects').https;
-//var fs = require('fs');
-
-
-fetch_tgtg_fav = function () {
-
-	var now = new Date();
-
-	if (now.getHours()<9){
-		return
-	}
-
-	var options = {
-		'method': 'POST',
-		'hostname': 'apptoogoodtogo.com',
-		'path': '/api/item/v6/',
-		'headers': {
-			'Authorization': 'Bearer e30.eyJzdWIiOiI5MTczMTI2IiwiZXhwIjoxNjEwMjE2NTI0LCJ0IjoiZXF5S0hveGFUeTJyQVRBMUcycEZSUTowOjEifQ.tGzjxlCA-uTCPIiBGfrq8iVxFK9LwiTAFMeg4-3narg',
-			'Content-Type': 'application/json'
-		},
-		'maxRedirects': 20
-	};
-
-	var req = https.request(options, function (res) {
-		var chunks = [];
-
-		res.on("data", function (chunk) {
-			chunks.push(chunk);
-		});
-
-		res.on("end", function (chunk) {
-			var body = Buffer.concat(chunks);
-			//console.log(body.toString());
-			items = JSON.parse(body.toString()).items;
-			//console.log(items[0].display_name);
-			stores_available = "";
-			console.log(Date());
-			for (let index = 0; index < items.length; index++) {
-				const store = items[index];
-				console.log(store.display_name + " => " + store.items_available);
-				if (store.items_available) {
-					stores_available = stores_available + "; " + store.display_name;
-					if (store.display_name.split(" ")[0] == "Pomponette") {
-						subs.forEach(pushSubscription => {
-							webpush.sendNotification(pushSubscription, JSON.stringify({ title: 'fav tgtg available', body: "Panier Pomponette à sg !" }));
-						});
-					}
-				}
-			}
-			/*subs.forEach(pushSubscription => {
-				webpush.sendNotification(pushSubscription, JSON.stringify({ title: 'fav tgtg available', body: stores_available }));
-			});*/
-		});
-
-		res.on("error", function (error) {
-			console.error(error);
-		});
-	});
-
-	var postData = JSON.stringify({ "user_id": "9173126", "origin": { "latitude": 45.75190208790065, "longitude": 4.830272620720394 }, "radius": 30, "page_size": 100, "page": 1, "discover": false, "favorites_only": true, "item_categories": [], "diet_categories": [], "pickup_earliest": null, "pickup_latest": null, "search_phrase": null, "with_stock_only": false, "hidden_only": false, "we_care_only": false });
-
-	req.write(postData);
-
-	req.end();
-}
-
-setInterval(fetch_tgtg_fav, 20 * 60 * 1000);
-
-
-
-//connection.end();
 
 app.use(bodyParser.urlencoded({
 	extended: true
@@ -101,8 +30,6 @@ app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 
 app.set('view engine', 'ejs');
 
-var subs = [];
-
 // VAPID keys should only be generated only once.
 //const vapidKeys = webpush.generateVAPIDKeys();
 const vapidKeys = {
@@ -118,9 +45,85 @@ webpush.setVapidDetails(
 	vapidKeys.privateKey
 );
 
-app.get('/', function (req, res) {
-	//res.sendStatus(200);
 
+var subs = [];
+var tokens = {
+    "access_token": "",
+    "refresh_token": ""
+};
+
+function refresh_token(old_refresh_token) {
+	var options = {
+		'method': 'POST',
+		'url': 'https://apptoogoodtogo.com/api/auth/v1/token/refresh',
+		'headers': {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ "refresh_token": old_refresh_token })
+	};
+	request(options, function (error, response) {
+		console.log('\n refresh token');
+		console.log(Date());
+		console.log('\n');
+		if (error) console.error(error);//throw new Error(error);
+		console.log(response.body);
+		return response.body;
+	});
+
+}
+
+async function fetch_favorite(access_token) {
+	var options = {
+		'method': 'POST',
+		'url': 'https://apptoogoodtogo.com/api/item/v6/',
+		'headers': {
+			'Authorization': 'Bearer ' + access_token,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ "user_id": "9173126", "origin": { "latitude": 45.75190208790065, "longitude": 4.830272620720394 }, "radius": 30, "page_size": 100, "page": 1, "discover": false, "favorites_only": true, "item_categories": [], "diet_categories": [], "pickup_earliest": null, "pickup_latest": null, "search_phrase": null, "with_stock_only": false, "hidden_only": false, "we_care_only": false })
+
+	};
+	request(options, function (error, response) {
+		console.log('\n access favorite https://apptoogoodtogo.com/api/item/v6/');
+		console.log(Date());
+		console.log('\n');
+		if (error) console.error(error);//throw new Error(error);
+		console.log(response.body);
+		return response.body;
+	});
+}
+
+
+async function fetch_and_notif_fav(access_token) {
+	console.log('\n' + Date() + '\n');
+
+	var items = await fetch_favorite(access_token)
+		.then(JSON.parse)
+		.then(function (fav) {
+			return fav.items;
+		})
+		.catch((error) => {
+			console.error(error);
+			return;
+		});
+
+	stores_available = "";
+	for (let index = 0; index < items.length; index++) {
+		const store = items[index];
+		console.log(store.display_name + " => " + store.items_available);
+		if (store.items_available) {
+			stores_available = stores_available + "; " + store.display_name;
+			if (store.display_name.split(" ")[0] == "Pomponette") {
+				subs.forEach(pushSubscription => {
+					webpush.sendNotification(pushSubscription, JSON.stringify({ title: 'fav tgtg available', body: "Panier Pomponette à sg !" }));
+				});
+			}
+		}
+	}
+}
+
+
+app.get('/', function (req, res) {
 	res.status(200).sendFile("html/home.html", { root: "./public" });
 });
 
@@ -132,6 +135,9 @@ app.post('/subscribe', (req, res) => {
 	}
 	res.send(req.body);
 });
+
+
+// ****** Inutile ******
 
 app.post('/broadcast_notif', (req, res) => {
 	console.log('/broadcast_notif');
@@ -190,3 +196,69 @@ var server = https.createServer({ key: fs.readFileSync('ssl/server.key'), cert: 
 server.listen(process.env.PORT || 443, () => {
 	console.log(`App Started on PORT ${process.env.PORT || 443}`);
 });
+
+
+
+/*
+fetch_tgtg_fav = function () {
+
+	var now = new Date();
+
+	if (now.getHours() < 9) {
+		return
+	}
+
+	var options = {
+		'method': 'POST',
+		'hostname': 'apptoogoodtogo.com',
+		'path': '/api/item/v6/',
+		'headers': {
+			'Authorization': 'Bearer e30.eyJzdWIiOiI5MTczMTI2IiwiZXhwIjoxNjEwMjE2NTI0LCJ0IjoiZXF5S0hveGFUeTJyQVRBMUcycEZSUTowOjEifQ.tGzjxlCA-uTCPIiBGfrq8iVxFK9LwiTAFMeg4-3narg',
+			'Content-Type': 'application/json'
+		},
+		'maxRedirects': 20
+	};
+
+	var req = https.request(options, function (res) {
+		var chunks = [];
+
+		res.on("data", function (chunk) {
+			chunks.push(chunk);
+		});
+
+		res.on("end", function (chunk) {
+			var body = Buffer.concat(chunks);
+			//console.log(body.toString());
+			items = JSON.parse(body.toString()).items;
+			//console.log(items[0].display_name);
+			stores_available = "";
+			console.log(Date());
+			for (let index = 0; index < items.length; index++) {
+				const store = items[index];
+				console.log(store.display_name + " => " + store.items_available);
+				if (store.items_available) {
+					stores_available = stores_available + "; " + store.display_name;
+					if (store.display_name.split(" ")[0] == "Pomponette") {
+						subs.forEach(pushSubscription => {
+							webpush.sendNotification(pushSubscription, JSON.stringify({ title: 'fav tgtg available', body: "Panier Pomponette à sg !" }));
+						});
+					}
+				}
+			}
+
+		});
+
+		res.on("error", function (error) {
+			console.error(error);
+		});
+	});
+
+	var postData = JSON.stringify({ "user_id": "9173126", "origin": { "latitude": 45.75190208790065, "longitude": 4.830272620720394 }, "radius": 30, "page_size": 100, "page": 1, "discover": false, "favorites_only": true, "item_categories": [], "diet_categories": [], "pickup_earliest": null, "pickup_latest": null, "search_phrase": null, "with_stock_only": false, "hidden_only": false, "we_care_only": false });
+
+	req.write(postData);
+
+	req.end();
+}
+
+setInterval(fetch_tgtg_fav, 20 * 60 * 1000);
+*/
--- public/js/home.js
+++ public/js/home.js
@@ -20,16 +20,16 @@ window.addEventListener('load', () => {
 });
 
 const requestNotificationPermission = async () => {
-    const permission = await window.Notification.requestPermission();
-    // value of permission can be 'granted', 'default', 'denied'
-    // granted: user has accepted the request
-    // default: user has dismissed the notification permission popup by clicking on x
-    // denied: user has denied the request.
-    if(permission !== 'granted'){
-        console.log('Permission not granted for Notification');
-    } else {
-	    console.log("Permission granted for Notification");
-    }
+	const permission = await window.Notification.requestPermission();
+	// value of permission can be 'granted', 'default', 'denied'
+	// granted: user has accepted the request
+	// default: user has dismissed the notification permission popup by clicking on x
+	// denied: user has denied the request.
+	if (permission !== 'granted') {
+		console.log('Permission not granted for Notification');
+	} else {
+		console.log("Permission granted for Notification");
+	}
 }
 
 // Register the Service Worker 
